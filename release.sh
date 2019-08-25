1ba9097a72f9de12765fc5894ab08ff044dfcd94
lueq <lueq@outlook.com>
2019-08-25 18:25:18 +0200

    Add docker push

--- release.sh
+++ release.sh
@@ -2,4 +2,5 @@
 
 dotnet build --runtime linux-x64 mongo.csproj
 dotnet publish --runtime linux-x64
-docker build --tag mongo-load:$1 .
+docker build --tag tacsreg.azurecr.io/mongo-load:$1 .
+docker push tacsreg.azurecr.io/mongo-load:$1
